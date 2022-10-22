497da0a7c7432f717699e82461730c98f5a20cee
anwarul <anwarulhoque032@gmail.com>
2022-10-22 10:05:46 +0600

    get and set city and cloud condition

--- js/app.js
+++ js/app.js
@@ -8,14 +8,25 @@ const loadTemperature = city => {
     .then(data => displayTemperature(data));
 }
 const displayTemperature = data => {
-    const temperature = document.getElementById('temperature');=
-    temperature.innerText = data.main.temp
+    // const temperature = document.getElementById('temperature');
+    // temperature.innerText = data.main.temp;
+    setInnerTextById('temperature', data.main.temp)
+    setInnerTextById('cloud-condition', data.weather[0].main)
+}
+
+const setInnerTextById = (id, text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
 }
 
 document.getElementById('btn-search').addEventListener('click', function(){
     const searchField = document.getElementById('search-field');
     const city = searchField.value;
+    // set city
+    document.getElementById('city').innerText = city;
+
     loadTemperature(city);
+    searchField.value ='';
 })
 
 
